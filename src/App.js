b899011f9f975667554c555376be8b9dcc6299d6
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-05-06 13:01:34 -0500

    Adds Main component, moves nav to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from "react";
-import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Portfolio from "./components/Portfolio";
 import "./App.css";
 
 function App() {
+  // Nav Links
+  const navLinks = [
+    { name: "Projects", href: "#projects" },
+    { name: "About", href: "#about" },
+    { name: "Contact", href: "#contact" },
+    { name: "Resume", href: "#resume" },
+  ];
+
   return (
     <div>
-      <Nav />
-      <Portfolio />
+      <nav className="navbar">
+        <div className="portfolio-title">
+          <h1>LIZ DIETERICH</h1>
+        </div>
+        <div className="nav-links">
+          <ul>
+            {navLinks.map((navLink) => (
+              <li>
+                <a href={navLink.href}>{navLink.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+// Define the nav bar links - Done
+// Map to display navbar - Done
+// Add event listener/click handler change
+// Pass the variable to the "Main component" for condintional rendering
+// Add conditional styling
---
+++ src/components/Main/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <p>
+        The About, Portfolio, Contact, and Resume components will be rendered
+        here-
+      </p>
+    </main>
+  );
+}
+
+export default Main;
