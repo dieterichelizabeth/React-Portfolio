2b91972f3047595a5f56a9a409d05a20da20b283
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-05-06 22:46:17 -0500

    Change nav-link color on click

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
+  // Set the current page as the value of "state"
+  const [currentPage, SetCurrentPate] = useState("About");
+
   // Nav Links
   const navLinks = [
     { name: "Projects", href: "#projects" },
@@ -10,6 +14,8 @@ function App() {
     { name: "Contact", href: "#contact" },
     { name: "Resume", href: "#resume" },
   ];
+
+  const navHandler = (name) => SetCurrentPate(name);
 
   return (
     <div>
@@ -19,14 +25,26 @@ function App() {
         </div>
         <div className="nav-links">
           <ul>
-            {navLinks.map((navLink) => (
-              <li>
-                <a href={navLink.href}>{navLink.name}</a>
+            {navLinks.map((link) => (
+              <li key={link.name}>
+                <a
+                  href={link.href}
+                  onClick={() => {
+                    navHandler(link.name);
+                  }}
+                  className={
+                    currentPage === link.name ? "navActive" : "navInactive"
+                  }
+                >
+                  {link.name}
+                </a>
               </li>
             ))}
           </ul>
         </div>
       </nav>
+
+      <Main />
 
       <Footer />
     </div>
@@ -37,6 +55,6 @@ export default App;
 
 // Define the nav bar links - Done
 // Map to display navbar - Done
-// Add event listener/click handler change
+// Add event listener/click handler change - Done
 // Pass the variable to the "Main component" for condintional rendering
 // Add conditional styling
