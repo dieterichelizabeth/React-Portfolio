f6014b44a037e75f100d33b611de35110371360f
Elizabeth Dieterich <dieterichelizabeth@gmail.com>
2022-11-16 22:04:17 -0600

    Puts Nav in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from "react";
-import Header from "./components/Header";
+import React, { useState } from "react";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
+  // Page State
+  const [currentPage, SetCurrentPate] = useState("Home");
+
+  // Nav Links
+  const navLinks = [
+    { name: "Portfolio", href: "#portfolio" },
+    { name: "About", href: "#about" },
+    { name: "Contact", href: "#contact" },
+    { name: "Resume", href: "#resume" },
+  ];
+
+  const navHandler = (name) => SetCurrentPate(name);
+
   return (
-    <div>
-      <Header />
+    <>
+      <header>
+        <nav className="navbar">
+          <a
+            href={"/"}
+            onClick={() => {
+              navHandler("Home");
+            }}
+            className="portfolio-title"
+          >
+            Liz Dieterich
+          </a>
+
+          <ul className="nav-links">
+            {navLinks.map((link) => (
+              <li key={link.name}>
+                <a
+                  href={link.href}
+                  onClick={() => {
+                    navHandler(link.name);
+                  }}
+                  className={
+                    currentPage === link.name ? "navActive" : "navInactive"
+                  }
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+
+      <Main currentPage={currentPage} />
+
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,9 +3,13 @@ import Portfolio from "../Portfolio";
 import About from "../About";
 import Contact from "../Contact";
 import Resume from "../Resume";
+import Home from "../Home";
 
 function Main({ currentPage }) {
   const activePage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
     if (currentPage === "Portfolio") {
       return <Portfolio />;
     }
